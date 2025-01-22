3465c9184ccfa222accec479f26ca961bae74927
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2025-01-22 10:39:27 +0800

    Update diy.sh

--- configs/diy.sh
+++ configs/diy.sh
@@ -23,7 +23,7 @@ fi
 [ -d dosbox ] && patch -p1 < ./configs/fit_for_dosbox_build.patch
 
 # fceux build
-[ -d fceux ] && patch -p1 < ./configs/fit_for_fecux_build.patch
+[ -d fceux ] && patch -p1 < ./configs/fit_for_fceux_build.patch
 
 # gambatte build
 [ -d gambatte ] && patch -p1 < ./configs/fit_for_gambatte_build.patch
