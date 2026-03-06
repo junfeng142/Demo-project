aad555668337e06272ad1663ec6513a8a579b5b6
junfeng142 <hero_cao@163.com>
2026-03-06 09:41:12 +0800

    update

--- configs/diy.sh
+++ configs/diy.sh
@@ -16,88 +16,9 @@ mkdir -p _opk
 # libretro build
 [ -f libretro-super.sh ] && patch -p1 < fit_for_libretro_build.patch
 
-# pixel-reader build
-[ -f resources/miyoo-package/assets/reader.png ] && patch -p1 < fit_for_reader_build.patch
-
-# gmenu2x build
-[ -f src/gmenu2x.cpp ] && patch -p1 < fit_for_gmenu2x_build.patch
-
-# fkgpiod build
-[ -f gpio_axp209.c ] && patch -p1 < fit_for_fkgpiod_build.patch
-
-# scummvm build
-[ -f icons/scummvm.ico ] && patch -p1 < fit_for_scummvm_build.patch
-
-# snes9x4d build
-[ -f snes-pad_icon_32.png ] && patch -p1 < fit_for_snes9x4d_build.patch
-
-# vbemu build
-[ -f vb.png ] && patch -p1 < fit_for_vbemu_build.patch
-
-# st-sdl build
-[ -f st.c ] && patch -p1 < fit_for_st-sdl_build.patch
-
-# dosbox build
-[ -d dosbox ] && patch -p1 < fit_for_dosbox_build.patch
-
-# fceux build
-[ -f fceux.png ] && patch -p1 < fit_for_fceux_build.patch
-
-# gambatte build
-[ -d gambatte_sdl ] && patch -p1 < fit_for_gambatte_build.patch
-
-# retroarch build
-[ -f retroarch.c ] && patch -p1 < fit_for_retroarch_build.patch
-
-# libsdl2 build
-[ -d SDL2 ] && patch -p1 < fit_for_sdl2_build.patch
-
 # kernel build
 [ -d kernel ] && patch -p1 < fit_for_kernel_build.patch
 
 # uboot build
 [ -d include/u-boot ] && patch -p1 < fit_for_uboot_build.patch
-
-# oswan build
-[ -d minizip ] && patch -p1 < fit_for_oswan_build.patch
-
-# crafti build
-[ -f worldtask.cpp ] && patch -p1 < fit_for_crafti_build.patch
-
-# ffplay build
-[ -d ffplay-gcw0 ] && patch -p1 < fit_for_ffplay_build.patch
-
-# openbor build
-[ -d openbor-6391 ] && patch -p1 < fit_for_openbor_build.patch
-
-# vba build
-[ -f vbanext.png ] && patch -p1 < fit_for_vba_build.patch
-
-# cannonball build
-if [ -d "cannonboard" ]; then
-    sed -i 's/FunKey-sdk/FunKey-sdk-2.3.0/g' Makefile.funkey
-    sed -i 's/musleabihf/gnueabihf/g' Makefile.funkey
-fi
-
-# gpsp build
-if [ -f "game_config.txt" ]; then
-    patch -p1 < fit_for_gpsp_build.patch
-    cp borders/border1.png _opk/border.png
-    cp game_config.txt _opk/
-fi
-
-# ngp build
-[ -f race-od.man.txt ] && patch -p1 < fit_for_ngp_build.patch
-
-# picodrive build
-if [ -f picodrive.map ]; then
-    patch -p1 < fit_for_picodrive_build.patch
-    mv platform/opendingux/data/skin _opk/
-fi
-
-# pocketsnes build
-if [ -d "pocketsnes" ]; then
-    patch -p1 < fit_for_pocketsnes_build.patch
-    cp pocketsnes/backdrop.png _opk/
-fi
 
