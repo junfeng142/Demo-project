531baf5ec820949547d5cd9b557281196f256d1c
junfeng142 <hero_cao@163.com>
2026-04-20 10:59:32 +0800

    gmu update

--- applications/gmu/run.sh
+++ applications/gmu/run.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+
+CONFIGDIR=$HOME/.config/gmu
+CONFIGFILE=$CONFIGDIR/gmu.conf
+if [ ! -d "$CONFIGFILE" ]; then
+  mkdir -p $CONFIGDIR
+fi
+
+if [ ! -f "$CONFIGFILE" ]; then
+  cp gmu.miyoo.conf $CONFIGFILE
+fi
+
 cd `dirname $0`
-SDL_NOMOUSE=1 LD_LIBRARY_PATH=libs/ ./gmu.bin -c gmu.miyoo.conf
+SDL_NOMOUSE=1 LD_LIBRARY_PATH=libs/ ./gmu.bin -c $CONFIGFILE
