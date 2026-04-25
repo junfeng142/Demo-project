488eb9e2a6136daeb20fd8a7a96b30d8b1f09f51
junfeng142 <hero_cao@163.com>
2026-04-25 18:56:29 +0800

    jinyong

--- applications/jinyong/run.sh
+++ applications/jinyong/run.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 mydir=`dirname "$0"`
-
 export LD_LIBRARY_PATH=$mydir/libs:$LD_LIBRARY_PATH
 
-cd $mydir
+if [ ! -f "$HOME/.jinyong/script/oldtalk.idx" ]; then
+    mkdir -p $HOME/.jinyong/script
+fi
 
+if [ ! -f "$HOME/.jinyong/data/r3.grp" ]; then
+    mkdir -p $HOME/.jinyong/data
+    cp libs/r1.grp libs/r2.grp libs/r3.grp $HOME/.jinyong/data/
+fi
+
+cd $mydir
 ./jysdllua
